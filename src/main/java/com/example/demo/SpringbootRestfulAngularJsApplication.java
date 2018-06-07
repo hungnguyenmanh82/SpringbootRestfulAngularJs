88c89e7eb23a54a5b2116fd586ce981933b8e1cb
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2018-06-07 10:56:18 +0700

    update comment

--- src/main/java/com/example/demo/SpringbootRestfulAngularJsApplication.java
+++ src/main/java/com/example/demo/SpringbootRestfulAngularJsApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://o7planning.org/en/11793/crud-example-with-spring-boot-rest-and-angularjs
+ *
+ */
 @SpringBootApplication
 public class SpringbootRestfulAngularJsApplication {
 
